7690b4cfaf81607b21991bec43e1156220b5abc6
JuliArlianto <32285721+JuliArlianto@users.noreply.github.com>
2018-03-27 11:47:33 +0800

    membuat attribut dan behavior

--- src/praktek01/Pakaian.java
+++ src/praktek01/Pakaian.java
@@ -10,5 +10,19 @@ package praktek01;
  * @author ijul
  */
 public class Pakaian {
+    String Warna;
+    String Ukuran;
+    String Bahan;
+    String Tipe;
+    String Merk;
+    
+    void tampildata(){
+        System.out.println("Warna   : "+Warna);
+        System.out.println("Ukuran  : "+Ukuran);
+        System.out.println("Bahan   : "+Bahan);
+        System.out.println("Tipe    : "+Tipe);
+        System.out.println("Merk    : "+Merk);
+        System.out.println(" ");
+    }
     
 }
